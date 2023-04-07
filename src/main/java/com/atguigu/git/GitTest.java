c701e17eecb544f3dbe64016b56b44875e46071b
fight <fight@jb.com>
2023-04-07 08:34:23 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author: 24577
+ * @DESCRIPTION:
+ * @DATE: 2023-04-07 8:32
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
